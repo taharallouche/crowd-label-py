197fc6908ab193614292d7feb1bd8bd820d19cce
taharallouche <tahar.allouche.pro@gmail.com>
2024-04-07 17:45:14 +0200

    feat: define aggregator classes

--- size_matters/aggregation/aggregators.py
+++ size_matters/aggregation/aggregators.py
@@ -1,72 +1,99 @@
 import numpy as np
 import pandas as pd
-
-from size_matters.utils.inventory import COLUMNS, RELIABILITY_BOUNDS, RULES
-
-
-def apply_standard_approval_aggregator(annotations: pd.DataFrame) -> pd.DataFrame:
-    aggregated_labels = _get_aggregated_labels(annotations)
-
-    return aggregated_labels
+from abc import ABC, abstractmethod
+from size_matters.utils.inventory import COLUMNS, RELIABILITY_BOUNDS
+from typing import Callable
 
 
-def apply_condorcet_aggregator(annotations: pd.DataFrame) -> pd.DataFrame:
-    vote_size = annotations.sum(axis=1)
-    reliabilities = (len(annotations.columns) - vote_size - 1) / (
-        len(annotations.columns) - 2
-    )
-    reliabilities = reliabilities.clip(
-        RELIABILITY_BOUNDS.lower, RELIABILITY_BOUNDS.upper
-    )
-    weights = np.log(reliabilities / (1 - reliabilities))
+class Aggregator(ABC):
+    _type: str
 
-    aggregated_labels = _get_aggregated_labels(annotations, weights)
+    @abstractmethod
+    def aggregate(self, annotations: pd.DataFrame, **kwargs) -> pd.DataFrame:
+        pass
 
-    return aggregated_labels
+    @property
+    def type(self) -> str:
+        return self._type
+
+    @staticmethod
+    def _get_aggregated_labels(
+        annotations: pd.DataFrame, weights: pd.Series | None = None
+    ) -> pd.DataFrame:
+        weighted_answers = annotations
+
+        if weights is not None:
+            weighted_answers = annotations.multiply(weights, axis="index")
+
+        weighted_scores = weighted_answers.groupby(COLUMNS.question, sort=False)[
+            annotations.columns
+        ].sum()
+
+        weighted_scores = weighted_scores.reindex(
+            annotations.index.get_level_values(COLUMNS.question).unique()
+        )
+
+        winning_alternatives = pd.Categorical(
+            weighted_scores.idxmax(axis=1),
+            categories=annotations.columns,
+            ordered=True,
+        )
+
+        aggregated_labels = pd.get_dummies(
+            winning_alternatives, columns=annotations.columns
+        )
+
+        return aggregated_labels
 
 
-def apply_mallow_aggregator(
-    annotations: pd.DataFrame,
-    distance: str = RULES.jaccard,
-) -> pd.DataFrame:
-    vote_size = annotations.sum(axis=1)
+class StandardApprovalAggregator(Aggregator):
+    _type: str = "Standard Approval Voting"
 
-    if distance == RULES.euclid:
-        weights = np.sqrt(vote_size + 1) - np.sqrt(vote_size - 1)
-    elif distance == RULES.jaccard:
-        weights = 1 / vote_size
-    elif distance == RULES.dice:
-        weights = 2 / (vote_size + 1)
-
-    aggregated_labels = _get_aggregated_labels(annotations, weights)
-
-    return aggregated_labels
+    def aggregate(self, annotations: pd.DataFrame, **kwargs) -> pd.DataFrame:
+        return self._get_aggregated_labels(annotations)
 
 
-def _get_aggregated_labels(
-    annotations: pd.DataFrame, weights: pd.Series | None = None
-) -> pd.DataFrame:
-    weighted_answers = annotations
+class CondorcetAggregator(Aggregator):
+    _type: str = "Condorcet Voting"
 
-    if weights is not None:
-        weighted_answers = annotations.multiply(weights, axis="index")
+    def aggregate(self, annotations: pd.DataFrame, **kwargs) -> pd.DataFrame:
+        vote_size = annotations.sum(axis=1)
+        reliabilities = (len(annotations.columns) - vote_size - 1) / (
+            len(annotations.columns) - 2
+        )
+        reliabilities = reliabilities.clip(
+            RELIABILITY_BOUNDS.lower, RELIABILITY_BOUNDS.upper
+        )
+        weights = np.log(reliabilities / (1 - reliabilities))
 
-    weighted_scores = weighted_answers.groupby(COLUMNS.question, sort=False)[
-        annotations.columns
-    ].sum()
+        return self._get_aggregated_labels(annotations, weights)
 
-    weighted_scores = weighted_scores.reindex(
-        annotations.index.get_level_values(COLUMNS.question).unique()
-    )
 
-    winning_alternatives = pd.Categorical(
-        weighted_scores.idxmax(axis=1),
-        categories=annotations.columns,
-        ordered=True,
-    )
+class MallowAggregator(Aggregator):
+    @property
+    @abstractmethod
+    def _weight_calculator(self) -> Callable[[pd.Series], pd.Series]:
+        pass
 
-    aggregated_labels = pd.get_dummies(
-        winning_alternatives, columns=annotations.columns
-    )
+    def aggregate(self, annotations: pd.DataFrame, **kwargs) -> pd.DataFrame:
+        vote_size = annotations.sum(axis=1)
+        weights = type(self)._weight_calculator(vote_size)
 
-    return aggregated_labels
+        return self._get_aggregated_labels(annotations, weights)
+
+
+class EuclidAggregator(MallowAggregator):
+    _type: str = "Euclidean Mallow Voting"
+    _weight_calculator = lambda vote_size: np.sqrt(  # noqa : E731
+        vote_size + 1
+    ) - np.sqrt(vote_size - 1)
+
+
+class JaccardAggregator(MallowAggregator):
+    _type: str = "Jaccard Mallow Voting"
+    _weight_calculator = lambda vote_size: 1 / vote_size  # noqa : E731
+
+
+class DiceAggregator(MallowAggregator):
+    _type: str = "Dice Mallow Voting"
+    _weight_calculator = lambda vote_size: 2 / (vote_size + 1)  # noqa : E731
