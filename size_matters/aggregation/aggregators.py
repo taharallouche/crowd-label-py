23f0421c6c18f4f335a78b060851767426a4f0a6
taharallouche <tahar.allouche.pro@gmail.com>
2024-05-08 15:42:15 +0200

    feat: redesign aggregator classes

--- size_matters/aggregation/aggregators.py
+++ size_matters/aggregation/aggregators.py
@@ -6,55 +6,59 @@ from typing import Callable
 
 
 class Aggregator(ABC):
-    _type: str
+    _name: str
 
     @abstractmethod
     def aggregate(self, annotations: pd.DataFrame, **kwargs) -> pd.DataFrame:
         pass
 
-    @property
-    def type(self) -> str:
-        return self._type
+    def __str__(self) -> str:
+        return self._name
 
+
+class VoterMixin:
     @staticmethod
-    def _get_aggregated_labels(
-        annotations: pd.DataFrame, weights: pd.Series | None = None
-    ) -> pd.DataFrame:
-        weighted_answers = annotations
+    def _get_aggregated_labels(votes: pd.DataFrame) -> pd.DataFrame:
+        scores = votes.groupby(COLUMNS.question, sort=False)[votes.columns].sum()
 
-        if weights is not None:
-            weighted_answers = annotations.multiply(weights, axis="index")
-
-        weighted_scores = weighted_answers.groupby(COLUMNS.question, sort=False)[
-            annotations.columns
-        ].sum()
-
-        weighted_scores = weighted_scores.reindex(
-            annotations.index.get_level_values(COLUMNS.question).unique()
-        )
+        scores = scores.reindex(votes.index.get_level_values(COLUMNS.question).unique())
 
         winning_alternatives = pd.Categorical(
-            weighted_scores.idxmax(axis=1),
-            categories=annotations.columns,
+            scores.idxmax(axis=1),
+            categories=votes.columns,
             ordered=True,
         )
 
-        aggregated_labels = pd.get_dummies(
-            winning_alternatives, columns=annotations.columns
-        )
+        aggregated_labels = pd.get_dummies(winning_alternatives, columns=votes.columns)
 
         return aggregated_labels
 
 
-class StandardApprovalAggregator(Aggregator):
-    _type: str = "Standard Approval Voting"
+class WeightedAggregator(Aggregator, VoterMixin):
+    @property
+    @abstractmethod
+    def _weight_calculator(self) -> Callable[[pd.Series], pd.Series]:
+        pass
 
     def aggregate(self, annotations: pd.DataFrame, **kwargs) -> pd.DataFrame:
-        return self._get_aggregated_labels(annotations)
+        vote_size = annotations.sum(axis=1)
+        weights = type(self)._weight_calculator(vote_size)
+
+        weighted_answers = annotations.multiply(weights, axis="index")
+
+        return self._get_aggregated_labels(weighted_answers)
 
 
-class CondorcetAggregator(Aggregator):
-    _type: str = "Condorcet Voting"
+class StandardApprovalAggregator(WeightedAggregator):
+    _name: str = "Standard Approval Voting"
+
+    _weight_calculator = lambda vote_size: pd.Series(  # noqa : E731
+        1 * len(vote_size), index=vote_size.index
+    )
+
+
+class CondorcetAggregator(VoterMixin, Aggregator):
+    _name: str = "Condorcet Voting"
 
     def aggregate(self, annotations: pd.DataFrame, **kwargs) -> pd.DataFrame:
         vote_size = annotations.sum(axis=1)
@@ -65,35 +69,23 @@ class CondorcetAggregator(Aggregator):
             RELIABILITY_BOUNDS.lower, RELIABILITY_BOUNDS.upper
         )
         weights = np.log(reliabilities / (1 - reliabilities))
+        weighted_answers = annotations.multiply(weights, axis="index")
 
-        return self._get_aggregated_labels(annotations, weights)
-
-
-class MallowAggregator(Aggregator):
-    @property
-    @abstractmethod
-    def _weight_calculator(self) -> Callable[[pd.Series], pd.Series]:
-        pass
-
-    def aggregate(self, annotations: pd.DataFrame, **kwargs) -> pd.DataFrame:
-        vote_size = annotations.sum(axis=1)
-        weights = type(self)._weight_calculator(vote_size)
-
-        return self._get_aggregated_labels(annotations, weights)
+        return self._get_aggregated_labels(weighted_answers)
 
 
-class EuclidAggregator(MallowAggregator):
-    _type: str = "Euclidean Mallow Voting"
+class EuclidAggregator(WeightedAggregator):
+    _name: str = "Euclidean Mallow Voting"
     _weight_calculator = lambda vote_size: np.sqrt(  # noqa : E731
         vote_size + 1
     ) - np.sqrt(vote_size - 1)
 
 
-class JaccardAggregator(MallowAggregator):
-    _type: str = "Jaccard Mallow Voting"
+class JaccardAggregator(WeightedAggregator):
+    _name: str = "Jaccard Mallow Voting"
     _weight_calculator = lambda vote_size: 1 / vote_size  # noqa : E731
 
 
-class DiceAggregator(MallowAggregator):
-    _type: str = "Dice Mallow Voting"
+class DiceAggregator(WeightedAggregator):
+    _name: str = "Dice Mallow Voting"
     _weight_calculator = lambda vote_size: 2 / (vote_size + 1)  # noqa : E731
