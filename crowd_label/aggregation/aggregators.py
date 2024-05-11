f1c22d4c01aba734ce87a63fc8f32f135b3e0df3
taharallouche <tahar.allouche.pro@gmail.com>
2024-05-11 19:18:20 +0200

    feat: parameterize reliability bounds

--- crowd_label/aggregation/aggregators.py
+++ crowd_label/aggregation/aggregators.py
@@ -4,18 +4,18 @@ from typing import Callable
 import numpy as np
 import pandas as pd
 
-from crowd_label.utils.inventory import COLUMNS, RELIABILITY_BOUNDS
+from crowd_label.utils.inventory import COLUMNS, DEFAULT_RELIABILITY_BOUNDS
 
 
 class Aggregator(ABC):
-	_name: str
+	__name: str
 
 	@abstractmethod
 	def aggregate(self, annotations: pd.DataFrame, **kwargs) -> pd.DataFrame:
 		pass
 
 	def __str__(self) -> str:
-		return self._name
+		return self.__name
 
 
 class VoterMixin:
@@ -52,7 +52,7 @@ class WeightedAggregator(Aggregator, VoterMixin):
 
 
 class StandardApprovalAggregator(WeightedAggregator):
-	_name: str = "Standard Approval Voting"
+	__name: str = "Standard Approval Aggregator"
 
 	_weight_calculator = lambda vote_size: pd.Series(  # noqa : E731
 		1 * len(vote_size), index=vote_size.index
@@ -60,7 +60,15 @@ class StandardApprovalAggregator(WeightedAggregator):
 
 
 class CondorcetAggregator(VoterMixin, Aggregator):
-	_name: str = "Condorcet Voting"
+	__name: str = "Condorcet Aggregator"
+
+	def __init__(
+		self,
+		lower_reliability_bound: float = DEFAULT_RELIABILITY_BOUNDS.lower,
+		upper_reliability_bound: float = DEFAULT_RELIABILITY_BOUNDS.upper,
+	):
+		self.lower_reliability_bound = lower_reliability_bound
+		self.upper_reliability_bound = upper_reliability_bound
 
 	def aggregate(self, annotations: pd.DataFrame, **kwargs) -> pd.DataFrame:
 		vote_size = annotations.sum(axis=1)
@@ -68,7 +76,7 @@ class CondorcetAggregator(VoterMixin, Aggregator):
 			len(annotations.columns) - 2
 		)
 		reliabilities = reliabilities.clip(
-			RELIABILITY_BOUNDS.lower, RELIABILITY_BOUNDS.upper
+			self.lower_reliability_bound, self.upper_reliability_bound
 		)
 		weights = np.log(reliabilities / (1 - reliabilities))
 		weighted_answers = annotations.multiply(weights, axis="index")
@@ -77,17 +85,17 @@ class CondorcetAggregator(VoterMixin, Aggregator):
 
 
 class EuclidAggregator(WeightedAggregator):
-	_name: str = "Euclidean Mallow Voting"
+	__name: str = "Euclidean Mallow Aggregator"
 	_weight_calculator = lambda vote_size: np.sqrt(  # noqa : E731
 		vote_size + 1
 	) - np.sqrt(vote_size - 1)
 
 
 class JaccardAggregator(WeightedAggregator):
-	_name: str = "Jaccard Mallow Voting"
+	__name: str = "Jaccard Mallow Aggregator"
 	_weight_calculator = lambda vote_size: 1 / vote_size  # noqa : E731
 
 
 class DiceAggregator(WeightedAggregator):
-	_name: str = "Dice Mallow Voting"
+	__name: str = "Dice Mallow Aggregator"
 	_weight_calculator = lambda vote_size: 2 / (vote_size + 1)  # noqa : E731
