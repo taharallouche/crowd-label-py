a6b63effb7abb2f4bf2b044f687be472e0a428bb
taharallouche <tahar.allouche.pro@gmail.com>
2024-05-18 15:14:36 +0200

    chore: tidy reproduce paper results

--- crowd_label/_reproduce_paper_results.py
+++ crowd_label/_reproduce_paper_results.py
@@ -2,22 +2,47 @@ from pathlib import Path
 
 import pandas as pd
 
-from crowd_label._evaluation.accuracy import compare_methods
+from crowd_label._evaluation.accuracy import compare_methods, plot_accuracies
 from crowd_label.utils.inventory import COLUMNS, DATASETS
 
-if __name__ == "__main__":  # pragma: no cover
+
+def _process_dataset() -> None:
+	dataset, max_voters, n_batch = _read_input_parameters()
+
+	annotations, groundtruth = _load_dataset(dataset)
+
+	accuracies = compare_methods(
+		annotations=annotations,
+		groundtruth=groundtruth,
+		max_voters=max_voters,
+		n_batch=n_batch,
+	)
+
+	plot_accuracies(accuracies)
+
+
+def _read_input_parameters():
 	dataset_name = input(f"Select a dataset [{'|'.join(DATASETS)}]: ")
-	assert dataset_name in DATASETS, "Invalid dataset"
+	if dataset_name not in DATASETS:
+		raise ValueError(
+			f"Invalid dataset {dataset_name} choose from  [{'|'.join(DATASETS)}]"
+		)
+
 	dataset = DATASETS[dataset_name]
 
 	max_voters = int(
 		input(f"Choose the maximum number of voters, max={dataset.nbr_voters}:")
 	)
-	assert max_voters <= dataset.nbr_voters, "Too many voters"
+	if max_voters > dataset.nbr_voters:
+		raise ValueError(f"Too many voters. Don't exceed {dataset.nbr_voters}")
 
 	n_batch = int(input("Choose the number of batches: "))
-	assert n_batch > 0, "Please choose a positive number of batches"
+	if n_batch <= 0:
+		raise ValueError("Please provide a positive number of batches")
+	return dataset, max_voters, n_batch
 
+
+def _load_dataset(dataset):
 	annotations = pd.read_csv(
 		Path(dataset.path).parent / "annotations.csv",
 		index_col=[COLUMNS.question, COLUMNS.voter],
@@ -26,9 +51,8 @@ if __name__ == "__main__":  # pragma: no cover
 		Path(dataset.path).parent / "ground_truth.csv", index_col=[COLUMNS.question]
 	)
 
-	compare_methods(
-		annotations=annotations,
-		groundtruth=groundtruth,
-		max_voters=max_voters,
-		n_batch=n_batch,
-	)
+	return annotations, groundtruth
+
+
+if __name__ == "__main__":  # pragma: no cover
+	_process_dataset()
