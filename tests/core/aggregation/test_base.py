51a7fa3b5aa0f4f2617978b139186108633f2c63
taharallouche <tahar.allouche.pro@gmail.com>
2024-10-26 15:25:54 +0200

    fix: jaccard edge case

--- tests/core/aggregation/test_base.py
+++ tests/core/aggregation/test_base.py
@@ -1,171 +1,65 @@
-from unittest.mock import patch
-
 import pandas as pd
 import pytest
 
-from hakeem.core.aggregation.base import Aggregator
 from hakeem.core.utils.inventory import COLUMNS
 
 
 @pytest.mark.ut
 def test_WeightedApprovalMixin_handles_empty_input():
-	# Given
-	from hakeem.core.aggregation.base import WeightedApprovalMixin
+    # Given
+    from hakeem.core.aggregation.base import WeightedApprovalMixin
 
-	annotations = pd.DataFrame(
-		columns=["a", "b"],
-		index=pd.MultiIndex.from_tuples((), names=[COLUMNS.question, COLUMNS.voter]),
-	)
+    annotations = pd.DataFrame(
+        columns=["a", "b"],
+        index=pd.MultiIndex.from_tuples((), names=[COLUMNS.question, COLUMNS.voter]),
+    )
 
-	# Then
-	result = WeightedApprovalMixin._get_aggregated_labels(
-		annotations, task_column=COLUMNS.question
-	)
+    # Then
+    result = WeightedApprovalMixin._aggregate_weighted_answers(
+        annotations, task_column=COLUMNS.question
+    )
 
-	# Then
-	assert result.empty
+    # Then
+    assert result.empty
 
 
 @pytest.mark.ut
 @pytest.mark.parametrize(
-	["weighted_votes", "expected_result"],
-	[
-		(
-			pd.DataFrame(
-				{
-					COLUMNS.question: ["q1", "q1"],
-					COLUMNS.voter: ["v1", "v2"],
-					"a": [1, 0],
-					"b": [0, 1],
-					"c": [1, 1],
-				}
-			).set_index([COLUMNS.question, COLUMNS.voter]),
-			pd.DataFrame(
-				[[False, False, True]],
-				columns=pd.CategoricalIndex(
-					["a", "b", "c"],
-					categories=["a", "b", "c"],
-					ordered=False,
-					dtype="category",
-				),
-				index=pd.Index(["q1"], name=COLUMNS.question),
-			),
-		),
-	],
+    ["weighted_votes", "expected_result"],
+    [
+        (
+            pd.DataFrame(
+                {
+                    COLUMNS.question: ["q1", "q1"],
+                    COLUMNS.voter: ["v1", "v2"],
+                    "a": [1, 0],
+                    "b": [0, 1],
+                    "c": [1, 1],
+                }
+            ).set_index([COLUMNS.question, COLUMNS.voter]),
+            pd.DataFrame(
+                [[False, False, True]],
+                columns=pd.CategoricalIndex(
+                    ["a", "b", "c"],
+                    categories=["a", "b", "c"],
+                    ordered=False,
+                    dtype="category",
+                ),
+                index=pd.Index(["q1"], name=COLUMNS.question),
+            ),
+        ),
+    ],
 )
 def test_Voter_Mixin_get_aggregated_labels_handles_one_question(
-	weighted_votes: pd.DataFrame, expected_result: pd.DataFrame
+    weighted_votes: pd.DataFrame, expected_result: pd.DataFrame
 ) -> None:
-	# Given
-	from hakeem.core.aggregation.base import WeightedApprovalMixin
+    # Given
+    from hakeem.core.aggregation.base import WeightedApprovalMixin
 
-	# When
-	result = WeightedApprovalMixin._get_aggregated_labels(
-		weighted_votes, task_column=COLUMNS.question
-	)
+    # When
+    result = WeightedApprovalMixin._aggregate_weighted_answers(
+        weighted_votes, task_column=COLUMNS.question
+    )
 
-	# Then
-	pd.testing.assert_frame_equal(expected_result, result)
-
-
-@pytest.mark.ut
-@pytest.mark.parametrize(
-	["annotations", "task_column", "worker_column", "expected_result"],
-	[
-		(
-			pd.DataFrame(
-				{
-					"task": ["q1", "q1"],
-					"worker": ["v1", "v2"],
-					"a": [1, 0],
-					"b": [0, 1],
-				}
-			).set_index(["task", "worker"]),
-			"task",
-			"worker",
-			pd.DataFrame(
-				{
-					"task": ["q1", "q1"],
-					"worker": ["v1", "v2"],
-					"a": [1, 0],
-					"b": [0, 1],
-				}
-			).set_index(["task", "worker"]),
-		),
-		(
-			pd.DataFrame(
-				{
-					"question": ["q1", "q1"],
-					"voter": ["v1", "v2"],
-					"a": [1, 0],
-					"b": [0, 1],
-				}
-			),
-			"question",
-			"voter",
-			pd.DataFrame(
-				{
-					"question": ["q1", "q1"],
-					"voter": ["v1", "v2"],
-					"a": [1, 0],
-					"b": [0, 1],
-				}
-			).set_index(["question", "voter"]),
-		),
-		(
-			pd.DataFrame(
-				{
-					"question": ["q1", "q1"],
-					"voter": ["v1", "v2"],
-					"a": [1, 0],
-					"b": [0, 1],
-				}
-			).set_index("question"),
-			"question",
-			"voter",
-			pd.DataFrame(
-				{
-					"question": ["q1", "q1"],
-					"voter": ["v1", "v2"],
-					"a": [1, 0],
-					"b": [0, 1],
-				}
-			).set_index(["question", "voter"]),
-		),
-		(
-			pd.DataFrame(
-				{
-					"extra_index_level": ["l1", "l2"],
-					"question": ["q1", "q1"],
-					"voter": ["v1", "v2"],
-					"a": [1, 0],
-					"b": [0, 1],
-				}
-			).set_index(["extra_index_level", "question"]),
-			"question",
-			"voter",
-			pd.DataFrame(
-				{
-					"question": ["q1", "q1"],
-					"voter": ["v1", "v2"],
-					"a": [1, 0],
-					"b": [0, 1],
-				}
-			).set_index(["question", "voter"]),
-		),
-	],
-)
-@patch.multiple(Aggregator, __abstractmethods__=set())
-def test_Aggregator__coerce_annotations(
-	annotations: pd.DataFrame,
-	task_column: str,
-	worker_column: str,
-	expected_result: pd.DataFrame,
-):
-	# When
-	result = Aggregator(
-		task_column=task_column, worker_column=worker_column
-	)._coerce_annotations(annotations)
-
-	# Then
-	pd.testing.assert_frame_equal(expected_result, result)
+    # Then
+    pd.testing.assert_frame_equal(expected_result, result)
--- tests/core/utils/test_utils.py
+++ tests/core/utils/test_utils.py
@@ -1,30 +1,34 @@
 import numpy as np
 import pytest
 
-from hakeem.core.utils.utils import get_mean_confidence_interval
-
 
 @pytest.mark.ut
 @pytest.mark.parametrize(
-	"data, confidence, expected",
-	[
-		(
-			np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
-			0.95,
-			(5.5, 3.3341494103866087, 7.665850589613392),
-		),
-		(
-			np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
-			0.99,
-			(5.5, 2.388519354409619, 8.611480645590381),
-		),
-		(
-			np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
-			0.90,
-			(5.5, 3.7449279866986926, 7.255072013301307),
-		),
-	],
+    ["data", "confidence", "expected"],
+    [
+        (
+            np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+            0.95,
+            (5.5, 3.3341494103866087, 7.665850589613392),
+        ),
+        (
+            np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+            0.99,
+            (5.5, 2.388519354409619, 8.611480645590381),
+        ),
+        (
+            np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+            0.90,
+            (5.5, 3.7449279866986926, 7.255072013301307),
+        ),
+    ],
 )
 def test_get_mean_confidence_interval(data, confidence, expected):
-	result = get_mean_confidence_interval(data, confidence)
-	assert result == pytest.approx(expected, abs=1e-6)
+    # Given
+    from hakeem.paper_results.evaluation.utils import get_mean_confidence_interval
+
+    # When
+    result = get_mean_confidence_interval(data, confidence)
+
+    # Then
+    np.testing.assert_allclose(result, expected, rtol=1e-6)
