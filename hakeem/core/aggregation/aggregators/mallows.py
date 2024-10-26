77944e82c51aaf588eb01ae9054023c78d997645
taharallouche <tahar.allouche.pro@gmail.com>
2024-10-26 11:55:12 +0200

    test: aggregators

--- hakeem/core/aggregation/aggregators/mallows.py
+++ hakeem/core/aggregation/aggregators/mallows.py
@@ -5,27 +5,28 @@ from hakeem.core.aggregation.base import WeightedAggregator
 
 
 class StandardApprovalAggregator(WeightedAggregator):
-	@staticmethod
-	def compute_weights(annotations: pd.DataFrame) -> pd.Series:
-		return pd.Series(1, index=annotations.index)
+    @staticmethod
+    def compute_weights(annotations: pd.DataFrame) -> pd.Series:
+        return pd.Series(1, index=annotations.index)
 
 
 class EuclidAggregator(WeightedAggregator):
-	@staticmethod
-	def compute_weights(annotations: pd.DataFrame) -> pd.Series:
-		vote_size = annotations.sum(axis=1)
-		return np.sqrt(vote_size + 1) - np.sqrt(vote_size - 1)
+    @staticmethod
+    def compute_weights(annotations: pd.DataFrame) -> pd.Series:
+        vote_size = annotations.sum(axis=1)
+        return np.sqrt(vote_size + 1) - np.sqrt(vote_size - 1)
 
 
 class JaccardAggregator(WeightedAggregator):
-	@staticmethod
-	def compute_weights(annotations: pd.DataFrame) -> pd.Series:
-		vote_size = annotations.sum(axis=1)
-		return 1 / vote_size
+    @staticmethod
+    def compute_weights(annotations: pd.DataFrame) -> pd.Series:
+        vote_size = annotations.sum(axis=1)
+        assert np.all(vote_size > 0), "Jaccard weights are not defined for empty votes"
+        return 1 / vote_size
 
 
 class DiceAggregator(WeightedAggregator):
-	@staticmethod
-	def compute_weights(annotations: pd.DataFrame) -> pd.Series:
-		vote_size = annotations.sum(axis=1)
-		return 2 / (vote_size + 1)
+    @staticmethod
+    def compute_weights(annotations: pd.DataFrame) -> pd.Series:
+        vote_size = annotations.sum(axis=1)
+        return 2 / (vote_size + 1)
